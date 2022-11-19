c619cbb1e8c1986e3369597c920d83215217f0db
David Morgan <52216154+studio-dm@users.noreply.github.com>
2022-11-18 23:44:08 -0500

    Update youTubePlayer.js

--- assets/youTubePlayer.js
+++ assets/youTubePlayer.js
@@ -9,3 +9,18 @@ function callVideo(videoID){
 const musicVideo = ["Iss9xKyqAeA", "syIUmrSJWAU", "u8pVZ5hTGJQ", "4aeETEoNfOg", "LCdltGg4EzM", "dVM8_jAL86w"];
 const random = Math.floor(Math.random() * musicVideo.length);
 callVideo(musicVideo[random]);
+
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla Lorem ipsum dummy text blabla Lorem ipsum dummy text blabla Lorem ipsum dummy text blabla.';
+var howFast = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("message").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, howFast);
+   
+  }
+  window.scrollTo(0, 0);
+  document.getElementById("message").innerHTML=="";
+}
